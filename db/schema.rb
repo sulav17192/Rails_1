2126a49e39006a3bbe2d57c4b120cff038045664
Sulav Aryal <aryalpurna6@gmail.com>
2025-12-30 16:56:29 +0545

    migrated the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_29_170506) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_30_110743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,6 +28,25 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_29_170506) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "order_id", null: false
+    t.decimal "price"
+    t.bigint "product_id", null: false
+    t.integer "quantity"
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "customer_id", null: false
+    t.decimal "total"
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
@@ -29,4 +54,20 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_29_170506) do
     t.integer "stock"
     t.datetime "updated_at", null: false
   end
+
+  create_table "shipping_details", force: :cascade do |t|
+    t.string "address"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.bigint "order_id", null: false
+    t.string "state"
+    t.datetime "updated_at", null: false
+    t.string "zip"
+    t.index ["order_id"], name: "index_shipping_details_on_order_id"
+  end
+
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
+  add_foreign_key "orders", "customers"
+  add_foreign_key "shipping_details", "orders"
 end
