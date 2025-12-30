8282b14bc06a21600829a2e371d546864d6be15c
Sulav Aryal <aryalpurna6@gmail.com>
2025-12-30 17:36:05 +0545

    added associations to all models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  belongs_to :category, optional: true   # optional: true because null is allowed for now
+    has_many :order_items, dependent: :nullify   # or :destroy if you prefer
 end
