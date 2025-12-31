f8216e420d641627916817c6d8d6532807ad672e
Sulav Aryal <aryalpurna6@gmail.com>
2025-12-31 19:57:14 +0545

    added callback to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,17 @@ class Order < ApplicationRecord
 
   validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :ordered_at, presence: true
+
+  before_create :say_before_create
+  after_update :say_after_update
+
+  private
+
+  def say_before_create
+    puts "Creating a new order for customer ID: #{customer_id}"
+  end
+
+  def say_after_update
+    puts "Order #{id} was just updated!"
+  end
 end
