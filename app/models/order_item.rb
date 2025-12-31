bb14f1e5d4c5d87d5a750484391d10f35ad3a5c5
Sulav Aryal <aryalpurna6@gmail.com>
2025-12-31 18:23:59 +0545

    validation on order_item

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,7 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  validates :unit_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 end
