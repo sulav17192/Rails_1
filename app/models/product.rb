9b05295d8a32b401e844041ec7a20280949f3b34
Sulav Aryal <aryalpurna6@gmail.com>
2025-12-31 18:22:21 +0545

    validation on product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :category, optional: true   # optional: true because null is allowed for now
     has_many :order_items, dependent: :nullify   # or :destroy if you prefer
+
+    validates : name, presence: true, uniqueness: true
+    vaildate : description, presence: true
+    validattes : price, presence: true, numericality: { greater_than_or_equal_to: 0}
 end
