e5ff127c32d52b978c2724729c8adb8a2f7d6a1b
Sulav Aryal <aryalpurna6@gmail.com>
2025-12-31 19:56:31 +0545

    added validation to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,7 @@
 class Customer < ApplicationRecord
   has_many :orders, dependent: :destroy
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
