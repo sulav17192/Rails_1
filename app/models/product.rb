5fae6d4ba2c6ebf35b92e1d8dffc651f61619806
Sulav Aryal <aryalpurna6@gmail.com>
2025-12-31 19:32:12 +0545

    Added callback to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,21 @@ class Product < ApplicationRecord
   belongs_to :category, optional: true   # optional: true because null is allowed for now
     has_many :order_items, dependent: :nullify   # or :destroy if you prefer
 
-    validates : name, presence: true, uniqueness: true
-    vaildate : description, presence: true
-    validattes : price, presence: true, numericality: { greater_than_or_equal_to: 0}
+validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+
+  before_save :say_before_save
+  after_create :say_after_create
+
+  private
+
+  def say_before_save
+    puts"Product is about to be saved: #{name}"
+  end
+
+  def say_after_create
+    puts" Product was created successfully"
+  end
 end
