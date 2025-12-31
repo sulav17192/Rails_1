27afbdf56d294ba6dac1b293d383d6d005be3263
Sulav Aryal <aryalpurna6@gmail.com>
2025-12-31 19:44:26 +0545

    Fixed error in order_item

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,14 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
-  validates :unit_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+ validates :unit_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+
+  after_save :say_item_saved
+
+  private
+
+  def say_item_saved
+    puts "OrderItem saved: #{quantity} x #{product.name}"
+  end
 end
