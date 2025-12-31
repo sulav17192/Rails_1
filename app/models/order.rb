ef2a975c5b5ef066c7d967917ee421ec94884567
Sulav Aryal <aryalpurna6@gmail.com>
2025-12-31 18:23:03 +0545

    validation on order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,7 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items, dependent: :destroy
   has_one :shipping_detail, dependent: :destroy
+
+  validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :ordered_at, presence: true
 end
