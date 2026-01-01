91a93792c0488393c5034cf182a1cf8ed5ff5f0c
Sulav Aryal <aryalpurna6@gmail.com>
2026-01-01 08:56:44 +0545

    Added scope in customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,6 @@ class Customer < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
+
+  scope :with_orders, -> {joins(:orders).distinct}
 end
